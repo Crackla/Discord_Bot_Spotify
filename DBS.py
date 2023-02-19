a7e98dcb03b46c7a733ad982c91014bbe0116324
Crackla <122633839+Crackla@users.noreply.github.com>
2023-02-19 23:37:28 +0100

    Update DBS.py
    -Removed unnecessary variables and functions
    -Added emojis to the replies
    -Prevents bot from replying to itself
    -Added RegEx module
    -Spotify link does not need to be at the beginning of the message anymore, the position within the messages is no longer relevant

--- DBS.py
+++ DBS.py
@@ -2,6 +2,7 @@ import nextcord
 from nextcord.ext import commands
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy
+import re
 
 
 playlist_id = "playlist_id"
@@ -17,37 +18,35 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 results = []
-check = []
 playlist_list = []
 
 @bot.event
 async def on_message(ctx):
     messages = await ctx.channel.history(limit=1).flatten()
+    if ctx.author == bot.user:
+        return
     for message in messages:
-        check.clear()
-        results.clear()
         if "https://open.spotify.com/" in message.content:
-            spotify_message = message.content.split()[0]
-            spotify_link = spotify_message.split("?")[0]
-            if spotify_link not in check:
-                check.append(spotify_link)
+            results.clear()
+            spotify_link_pattern = r"(?P<url>https?://(?:open\.|play\.)?spotify\.com/track/[a-zA-Z0-9?=]+)"
+            spotify_links = re.findall(spotify_link_pattern, message.content)
+            spotify_string = " ".join(spotify_links)
+            spotify_url = spotify_string.split("?")[0]
+            results.append(spotify_url)
             playlist = sp.playlist_items(
                 playlist_id, fields=None, limit=100, offset=0, market=None, additional_types=("track",))
             for item in playlist["items"]:
-                track = item.get("track")
-                if track:
-                    url_id = (track["id"])
-                    track_url = "https://open.spotify.com/track/" + url_id
-                    if track_url not in playlist_list:
-                        playlist_list.append(track_url)
-            for song in check:
+                track = item.get("track", "id")
+                url_id = (track["id"])
+                track_url = "https://open.spotify.com/track/" + url_id
+                if track_url not in playlist_list:
+                    playlist_list.append(track_url)
+            for song in results:
                 if song not in playlist_list:
-                    results.append(song)
-                    if results not in playlist_list:
-                        sp.playlist_add_items(playlist_id, items=results, position=None)
-                        await message.reply("I added the song to the playlist")
+                    sp.playlist_add_items(playlist_id, items=results, position=None)
+                    await message.reply("I added the song to the playlist \U0001F600")
                 if song in playlist_list:
-                    await message.reply("Song is already in the playlist")
+                    await message.reply("The song is already in the playlist \U0001F605")
 
 
 
