53ca12778569a1dc46c90af3b8eb3ce3ec9f814e
Crackla <122633839+Crackla@users.noreply.github.com>
2023-02-20 18:49:15 +0100

    Update DBS.py
    -Can find multiple Spotify links in the same message now
    -Added the referenced song link to the reply
    -Changed "https://open.spotify.com/" to "spotify.com/track" for better accuracy

--- DBS.py
+++ DBS.py
@@ -20,19 +20,22 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 results = []
 playlist_list = []
 
+
 @bot.event
 async def on_message(ctx):
     messages = await ctx.channel.history(limit=1).flatten()
     if ctx.author == bot.user:
         return
     for message in messages:
-        if "https://open.spotify.com/" in message.content:
+        if "spotify.com/track" in message.content:
             results.clear()
             spotify_link_pattern = r"(?P<url>https?://(?:open\.|play\.)?spotify\.com/track/[a-zA-Z0-9?=]+)"
             spotify_links = re.findall(spotify_link_pattern, message.content)
-            spotify_string = " ".join(spotify_links)
-            spotify_url = spotify_string.split("?")[0]
-            results.append(spotify_url)
+            for spotify_link in spotify_links:
+                spotify_string = "".join(spotify_link)
+                spotify_url = spotify_string.split("?")[0]
+                if spotify_url not in results:
+                  results.append(spotify_url)
             playlist = sp.playlist_items(
                 playlist_id, fields=None, limit=100, offset=0, market=None, additional_types=("track",))
             for item in playlist["items"]:
@@ -42,10 +45,10 @@ async def on_message(ctx):
                     playlist_list.append(track_url)
             for song in results:
                 if song not in playlist_list:
-                    sp.playlist_add_items(playlist_id, items=results, position=None)
-                    await message.reply("I added the song to the playlist \U0001F600")
+                    sp.playlist_add_items(playlist_id, items=[song], position=None)
+                    await message.reply("I added this song to the playlist \U0001F600" + "\n" + song)
                 if song in playlist_list:
-                    await message.reply("The song is already in the playlist \U0001F605")
+                    await message.reply("This song is already in the playlist \U0001F605" + "\n" + song)
 
 
 
